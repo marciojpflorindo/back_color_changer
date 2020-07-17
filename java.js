d8740b826f6130f951f36503295640530203a78d
Márcio Florindo <61458507+marciojpflorindo@users.noreply.github.com>
2020-07-17 08:49:19 +0100

    changed color; from JQuery to vanilla Js

--- java.js
+++ java.js
@@ -1,21 +1,9 @@
-// function mixedColors() {
-//   var colorRed = Math.floor(Math.random() * 256);
-//   var colorBlue = Math.floor(Math.random() * 256);
-//   var colorGreen = Math.floor(Math.random() * 256);
-//   var colors = "rgb" + "(" + colorRed + ", " + colorBlue + ", " + colorGreen + ")";
-//   document.body.style.backgroundColor = colors;
-// }
-//
-//
-//
-// document.querySelector("button").addEventListener("click", mixedColors);
-
-$("button").on("click", mixedColors)
-
 function mixedColors() {
-  var red = Math.floor(Math.random() * 256);
-  var green = Math.floor(Math.random() * 256);
-  var blue = Math.floor(Math.random() * 256);
-  var rgbColors = "rgb(" + red + ", " + green + ", " + blue + ")";
-  $("body").css("background-color", rgbColors);
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let rgbColors = "rgb(" + red + ", " + green + ", " + blue + ")";
+  document.body.style.backgroundColor = rgbColors;
+  let title = document.querySelector('.title');
+  title.style.backgroundColor = rgbColors;
 }
